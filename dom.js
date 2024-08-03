22c007745f4b4ad87e0865e2c689ba620fedf51a
Rahul Kumar <techaffairs4@gmail.com>
2024-08-03 06:32:50 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,7 @@
+// Accessing a button element
+const myButton = document.getElementById('myButton');
+
+// Adding a click event listener
+myButton.addEventListener('click', function() {
+  alert('Button Clicked! 🌟');
+});
